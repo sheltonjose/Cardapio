65a3e2a94ef4762cf4e61449cc498f5f03a180fc
Shelton <sheltonjose02@gmail.com>
2026-03-01 01:11:37 +0200

    Update FoodController.java

--- src/main/java/com/example/Cardapio/Controller/FoodController.java
+++ src/main/java/com/example/Cardapio/Controller/FoodController.java
@@ -1,16 +1,27 @@
 package com.example.Cardapio.Controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.Cardapio.FoodRepository;
+import com.example.Cardapio.entity.Food;
+
 @RestController
 @RequestMapping("food")
 public class FoodController {
+
+    @Autowired
+    private FoodRepository foodRepository;
     
     @GetMapping
-    public void getAll(){
+    public List<Food> getAll(){
 
+    List<Food> foodlist= foodRepository.findAll();
+    return foodlist;
         
 
     }
