a6106c2866969b2421c77284e9066cf7f45635e6
Shelton <sheltonjose02@gmail.com>
2026-03-01 00:43:01 +0200

    Update food.java

--- src/main/java/com/example/Cardapio/entity/food.java
+++ src/main/java/com/example/Cardapio/entity/food.java
@@ -1,5 +1,10 @@
 package com.example.Cardapio.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Table(name = "foods")
+@Entity(name="foods")
 public class food {
 
 }
