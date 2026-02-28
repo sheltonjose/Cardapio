053d2dd9c0e4716fd4505570d326c921a66c2262
Shelton <sheltonjose02@gmail.com>
2026-03-01 00:50:30 +0200

    Update food.java

--- src/main/java/com/example/Cardapio/entity/food.java
+++ src/main/java/com/example/Cardapio/entity/food.java
@@ -1,10 +1,29 @@
 package com.example.Cardapio.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+
+
+
 
 @Table(name = "foods")
 @Entity(name="foods")
 public class food {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String imageUrl;
+    private Integer price;
+
+
+
+
+
 
 }
