9387c66db924f16e411a674c0420f74229b83a3c
Shelton <sheltonjose02@gmail.com>
2026-03-01 00:08:01 +0200

    Update FoodController.java

--- src/main/java/com/example/Cardapio/Controller/FoodController.java
+++ src/main/java/com/example/Cardapio/Controller/FoodController.java
@@ -1,6 +1,18 @@
 package com.example.Cardapio.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("food")
 public class FoodController {
+    
+    @GetMapping
+    public void getAll(){
+
+
+    }
 
 
 }
