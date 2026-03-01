3407c154741edaa06d2a800c1da228b8e92a99f9
Shelton <sheltonjose02@gmail.com>
2026-03-02 00:47:04 +0200

    Update FoodResponseDTO.java

--- src/main/java/com/example/Cardapio/FoodResponseDTO.java
+++ src/main/java/com/example/Cardapio/FoodResponseDTO.java
@@ -1,14 +1,14 @@
 package com.example.Cardapio;
 
-public record FoodResponseDTO(
-    Long id,
-    String title,
-    String imageUrl,
-    Integer price
+import com.example.Cardapio.entity.Food;
+
+public record FoodResponseDTO( Long id,String title,String imageUrl,Integer price
 ) {
 
+    public FoodResponseDTO(Food food){
+       this(food.getId(),food.getTitle(),food.getImageUrl(),food.getPrice());
+    }
 
 
 
-    
 } 
