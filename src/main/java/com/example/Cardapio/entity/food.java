56639aa8d5f038b6e4c5f499107227d7b9db7da2
Shelton <sheltonjose02@gmail.com>
2026-03-02 00:40:20 +0200

    Update food.java

--- src/main/java/com/example/Cardapio/entity/food.java
+++ src/main/java/com/example/Cardapio/entity/food.java
@@ -5,12 +5,20 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 
 
 
 @Table(name = "foods")
 @Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class Food {
 
 
