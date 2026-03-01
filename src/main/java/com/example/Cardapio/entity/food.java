715f798323b90a47b316fc6f024fe6676eac59b2
Shelton <sheltonjose02@gmail.com>
2026-03-02 00:29:56 +0200

    Update food.java

--- src/main/java/com/example/Cardapio/entity/food.java
+++ src/main/java/com/example/Cardapio/entity/food.java
@@ -21,33 +21,6 @@ public class Food {
     private String imageUrl;
     private Integer price;
 
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public String getTitle() {
-        return title;
-    }
-    public void setTitle(String title) {
-        this.title = title;
-    }
-    public String getImageUrl() {
-        return imageUrl;
-    }
-    public void setImageUrl(String imageUrl) {
-        this.imageUrl = imageUrl;
-    }
-    public Integer getPrice() {
-        return price;
-    }
-    public void setPrice(Integer price) {
-        this.price = price;
-    }
-
-
-
 
 
 
